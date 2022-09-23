57014e4de3d06b7b7920817a61404168d6f0676b
Your Name <you@example.com>
2022-09-24 02:33:32 +0300

    declared placeholder into require source

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -37,7 +37,7 @@ const ItemPreview = (props) => {
           <img
             alt="item"
             src={item.image}
-            Image source={ item.img ? { uri: placeholder.png } : require('../images/dummy.png')} 
+            Image source={ item.img ? { uri: fr} : require('../frontend/public/placeholder.png')} 
             className="card-img-top item-img"
             style={{ borderRadius: "20px" }}
           />
