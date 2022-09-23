73fa6c196a46f37f9d7d0e53cdcaf6837d37ca40
Your Name <you@example.com>
2022-09-24 01:54:26 +0300

    added image.src in item preview

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -18,48 +18,50 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const ItemPreview = (props) => {
-  const item = props.item;
+  const img = document.querySelector("img"); 
+  img.src = "placeholder.png";
+      const item = props.item;
 
-  const handleClick = (ev) => {
-    ev.preventDefault();
-    if (item.favorited) {
-      props.unfavorite(item.slug);
-    } else {
-      props.favorite(item.slug);
-    }
-  };
+      const handleClick = (ev) => {
+        ev.preventDefault();
+        if (item.favorited) {
+          props.unfavorite(item.slug);
+        } else {
+          props.favorite(item.slug);
+        }
+      };
 
-  return (
-    <div
-      className="card bg-dark border-light p-3"
-      style={{ borderRadius: "20px" }}
-    >
-      <img
-        alt="item"
-        src={placeholder.png}
-        className="card-img-top item-img"
-        style={{ borderRadius: "20px" }}
-      />
-      <div className="card-body">
-        <Link to={`/item/${item.slug}`} className="text-white">
-          <h3 className="card-title">{item.title}</h3>
-          <p className="card-text crop-text-3">{item.description}</p>
-        </Link>
-        <div className="d-flex flex-row align-items-center pt-2">
-          <Link to={`/@${item.seller.username}`} className="flex-grow-1">
-            <img
-              src={item.seller.image}
-              alt={item.seller.username}
-              className="user-pic rounded-circle pr-1"
-            />
-          </Link>
-          <button className="btn btn-outline-secondary" onClick={handleClick}>
-            <i className="ion-heart"></i> {item.favoritesCount}
-          </button>
+      return (
+        <div
+          className="card bg-dark border-light p-3"
+          style={{ borderRadius: "20px" }}
+        >
+          <img
+            alt="item"
+            src={item.image}
+            className="card-img-top item-img"
+            style={{ borderRadius: "20px" }}
+          />
+          <div className="card-body">
+            <Link to={`/item/${item.slug}`} className="text-white">
+              <h3 className="card-title">{item.title}</h3>
+              <p className="card-text crop-text-3">{item.description}</p>
+            </Link>
+            <div className="d-flex flex-row align-items-center pt-2">
+              <Link to={`/@${item.seller.username}`} className="flex-grow-1">
+                <img
+                  src={item.seller.image}
+                  alt={item.seller.username}
+                  className="user-pic rounded-circle pr-1"
+                />
+              </Link>
+              <button className="btn btn-outline-secondary" onClick={handleClick}>
+                <i className="ion-heart"></i> {item.favoritesCount}
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      );
 };
 
 export default connect(() => ({}), mapDispatchToProps)(ItemPreview);
