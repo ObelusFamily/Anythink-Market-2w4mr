87ff12417bc6190d8712c6c21bc8dbcd0a88e710
Your Name <you@example.com>
2022-10-11 16:17:23 +0300

    nfh

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -37,7 +37,7 @@ const ItemPreview = (props) => {
           <img
             alt="item"
             src={item.image}
-            Image source={placeholder.png.props.imageName}
+            Image source={this.props.imageName}
             className="card-img-top item-img"
             style={{ borderRadius: "20px" }}
           />
